67b0e08579b6a2daf2f1c385ecd9de24e71c2d23
SandeeepS <sandeepsanthosh3303@gmail.com>
2024-04-17 17:18:02 +0530

    added css and added todo basic functions

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import './Todo.css'
+import { useState } from 'react'
 
 function Todo() {
+
+    const [todo,setTodo] = useState('')
+    const [todos,setTodos] = useState([]);
+
+    const handleSubmit = (e) => {
+            e.preventDefault();
+    }
+
+    const addTodo = () => {
+        setTodos([...todos,todo])
+        console.log(todos);
+    }
   return (
-    <div>
-        <h1>Hello World</h1>
+    <div className='container'>
+        <h1>TODO APP</h1>
+        <form action="" className='form-group' onSubmit={handleSubmit}>
+            <input type="text" value={todo} placeholder='Enter you todo' className='form-control' onChange={(event)=> setTodo(event.target.value)} /> 
+            
+            <button onClick={addTodo}>ADD</button>
+        </form>
+
+        <div className='list'>
+            <ul>
+                <li className='form-control' >First</li>
+                <li className='form-control' >Second</li>
+                <li className='form-control' >third</li>
+            </ul>
+        </div>
     </div>
   )
 }
