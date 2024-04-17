04894603d4f3237a8c842e71822207e2631a2a00
SandeeepS <sandeepsanthosh3303@gmail.com>
2024-04-17 16:38:51 +0530

    creating a todo app

--- src/App.js
+++ src/App.js
@@ -1,42 +1,12 @@
-import './App.css';
-import Header from './components/Header';
-import React,{useState} from 'react';
-import Employee from './Employee';
-import Counter from './Counter';
+import Todo from './components/Todo';
+
 
 function App() {
-
-      const data = 'Sandeep';
-      const [count,setCount] = useState(0)
-
-      const addCount = () => {
-          setCount(count+1)
-      }
-
-      let emp = [
-        {name:'Sandeep',age:21},
-        {name:'Arjun',age:21},
-        {name:'Unni',age:24},
-      ]
       return (
         <div>
-            <Header data={data}/>
-            <p>this is a sample data {data}</p>
-            <button onClick={addCount}>Add</button>
-            { 
-              emp.map((obj,index) => {
-                return(
-                    <Employee key={index}  name={obj.name} age={obj.age} />
-                )
-              })
-            }
-            
-            <Counter />
-
+          <Todo />
         </div>
-      );
+      )
 }
-
-
 
 export default App;
