8725531f01c05e82f455de54f290a3d82997287c
SandeeepS <sandeepsanthosh3303@gmail.com>
2024-04-18 09:07:42 +0530

    updated todi list

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,38 +1,75 @@
-import React from 'react'
-import './Todo.css'
-import { useState } from 'react'
+import React from "react";
+import "./Todo.css";
+import { useState, useRef, useEffect } from "react";
+import { IoMdDoneAll } from "react-icons/io";
+import { FiEdit } from "react-icons/fi";
+import { MdDelete } from "react-icons/md";
 
 function Todo() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
 
-    const [todo,setTodo] = useState('')
-    const [todos,setTodos] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
-    const handleSubmit = (e) => {
-            e.preventDefault();
-    }
+  const addTodo = () => {
+    setTodos([...todos, {list:todo,id : Date.now()}]);
+    console.log(todos);
+    setTodo("");
+  };
 
-    const addTodo = () => {
-        setTodos([...todos,todo])
-        console.log(todos);
-    }
+  const inputRef = useRef("null");
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
+ const onDelete = (id) => {
+   setTodos( todos.filter((todo)=> todo.id !== id) )
+ }
+
   return (
-    <div className='container'>
-        <h1>TODO APP</h1>
-        <form action="" className='form-group' onSubmit={handleSubmit}>
-            <input type="text" value={todo} placeholder='Enter you todo' className='form-control' onChange={(event)=> setTodo(event.target.value)} /> 
-            
-            <button onClick={addTodo}>ADD</button>
-        </form>
+    <div className="container">
+      <h1>TODO APP</h1>
+      <form action="" className="form-group" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={todo}
+          ref={inputRef}
+          placeholder="Enter you todo"
+          className="form-control"
+          onChange={(event) => setTodo(event.target.value)}
+        />
 
-        <div className='list'>
-            <ul>
-                <li className='form-control' >First</li>
-                <li className='form-control' >Second</li>
-                <li className='form-control' >third</li>
-            </ul>
-        </div>
+        <button onClick={addTodo}>ADD</button>
+      </form>
+
+      <div className="list">
+        <ul>
+          {todos.map((todo) => (
+            <li className="list-items">
+              <div className="list-item-list">{todo.list}</div>
+              <span>
+                <IoMdDoneAll
+                  className="list-item-icons"
+                  id="complete"
+                  title="Complete"
+                />
+                <FiEdit className="list-item-icons" id="edit" title="Edit" />
+                <MdDelete
+                  className="list-item-icons"
+                  id="delete"
+                  title="Delete"
+                  onClick={()=> onDelete(todo.id)}
+                />
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Todo
+export default Todo;
