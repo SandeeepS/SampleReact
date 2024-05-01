fd271d7b3c790613f1680a35b6cf94fb58f8ee84
SandeeepS <sandeepsanthosh3303@gmail.com>
2024-05-01 10:47:24 +0530

    delted unwanted contents

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -8,36 +8,51 @@ import { MdDelete } from "react-icons/md";
 function Todo() {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
-  const [editid,setEditId] = useState(0);
+  const [editid, setEditId] = useState(0);
+  const [showError, setShowError] = useState(false);
+  const [showError2, setShowError2] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
   };
 
-const addTodo = () => {
-    if(todo === ' '){
-      setTimeout(()=>{
-        setTodo("Please enter a valid list")
-      },3000)
-       
-    }else{
-    
-        if(editid){
-            const updatedTodos = todos.map((todoItem) => {
-                if (todoItem.id === editid) {
-                  return { ...todoItem, list: todo, status: todoItem.status };
-                }
-                return todoItem;
-              });
-              setTodos(updatedTodos);
-              setEditId(0); 
-        }else{
-            setTodos([{ list: todo, id: Date.now(), status: false }, ...todos]);
-
-        }
-        setTodo("");
+  const addTodo = () => {
+    if (todo.trim() === "") {
+      setShowError(true);
+      setTimeout(() => {
+        setShowError(false);
+      }, 3000);
+      return;
     }
-}
+    console.log(todos);
+    const isExist = todos.find((list) => list.list === todo);
+    console.log("if exist?" + isExist);
+    if (isExist) {
+      setShowError2(true);
+      setTimeout(() => {
+        setShowError2(false);
+      }, 3000);
+      return;
+    }
+
+    if (todo !== " ") {
+      if (editid) {
+        const updatedTodos = todos.map((todoItem) => {
+          if (todoItem.id === editid) {
+            return { ...todoItem, list: todo, status: todoItem.status };
+          }
+          return todoItem;
+        });
+        setTodos(updatedTodos);
+        setEditId(0);
+      } else {
+        console.log("todo value is " + todo);
+        setTodos([{ list: todo, id: Date.now(), status: false }, ...todos]);
+      }
+      console.log("making empty");
+      setTodo("");
+    }
+  };
 
   const inputRef = useRef("null");
 
@@ -46,28 +61,27 @@ const addTodo = () => {
   });
 
   //function to delete the list
- const onDelete = (id) => {
-   setTodos( todos.filter((todo)=> todo.id !== id) )
- }
+  const onDelete = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
 
+  //function to complete
+  const onComplete = (id) => {
+    let complete = todos.map((list) => {
+      if (list.id === id) {
+        return { ...list, status: !list.status };
+      }
+      return list;
+    });
+    setTodos(complete);
+  };
 
- //function to complete
- const onComplete = (id) => {
-    let complete = todos.map((list)=>{
-        if(list.id === id){
-            return ({...list,status: !list.status})
-        }
-        return list
-    })
-    setTodos(complete)
-
- }
-
- const onEdit = (id)=>{
-    const editedList = todos.find((list) => list.id === id)
-    setTodo(editedList.list)
-    setEditId(editedList.id)
- }
+  //function to edit
+  const onEdit = (id) => {
+    const editedList = todos.find((list) => list.id === id);
+    setTodo(editedList.list);
+    setEditId(editedList.id);
+  };
 
   return (
     <div className="container">
@@ -75,41 +89,50 @@ const addTodo = () => {
       <form action="" className="form-group" onSubmit={handleSubmit}>
         <input
           type="text"
-         
           ref={inputRef}
+          value={todo}
           placeholder="Enter you todo"
           className="form-control"
           onChange={(event) => setTodo(event.target.value)}
         />
 
-
-        <button onClick={addTodo}>{editid ? 'EDIT' : 'ADD'}</button>
+        <button onClick={addTodo}>{editid ? "EDIT" : "ADD"}</button>
       </form>
-     
+
       <div className="list">
-        <h4>{todo}</h4>
+        {showError && (
+          <div className="error-message"> Please enter a valid todo ! </div>
+        )}
+        {showError2 && (
+          <div className="error-message"> List already exist </div>
+        )}
         <ul>
           {todos.map((todo) => (
-            <li className="list-items">
-              <div className="list-item-list" id={todo.status ? "list-item" : ""} >{todo.list}</div>
+            <li className="list-items" key={todo.id}>
+              <div
+                className="list-item-list"
+                id={todo.status ? "list-item" : ""}
+              >
+                {todo.list}
+              </div>
               <span>
                 <IoMdDoneAll
                   className="list-item-icons"
                   id="complete"
                   title="Complete"
-                  onClick={()=> onComplete(todo.id)}
+                  onClick={() => onComplete(todo.id)}
                 />
-                <FiEdit 
-                  className="list-item-icons" 
-                  id="edit" 
+                <FiEdit
+                  className="list-item-icons"
+                  id="edit"
                   title="Edit"
-                  onClick={()=> onEdit(todo.id)}
+                  onClick={() => onEdit(todo.id)}
                 />
                 <MdDelete
                   className="list-item-icons"
                   id="delete"
                   title="Delete"
-                  onClick={()=> onDelete(todo.id)}
+                  onClick={() => onDelete(todo.id)}
                 />
               </span>
             </li>
